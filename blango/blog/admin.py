d426ca104c470ff19ce64f714fada9727714dc19
hatim-the-dark-knight <farhathsulthana2001@gmail.com>
2022-06-05 09:00:22 +0530

    Finish generic relationships

--- blango/blog/admin.py
+++ blango/blog/admin.py
@@ -1,6 +1,7 @@
 from re import S
+from xml.etree.ElementTree import Comment
 from django.contrib import admin
-from blog.models import Tag, Post
+from blog.models import Tag, Post, Comment
 
 # Register your models here.
 admin.site.register (Tag)
@@ -10,3 +11,5 @@ class PostAdmin (admin.ModelAdmin):
     list_display = ('slug', 'published_at')
 
 admin.site.register (Post, PostAdmin)
+
+admin.site.register (Comment)
